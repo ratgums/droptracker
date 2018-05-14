0ec1560298b62958909d29f1e19699afca400464
Scott Foster <scottgfoster0@yahoo.com>
2018-05-14 15:50:59 -0700

    OSRS Boss Drop Tracker
    basic ui

---
+++ main.js
@@ -0,0 +1,572 @@
+var ely = {
+  name : "Elysian Sigil",
+  id: 1,
+  drops: 0,
+  amount: 1,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+var arcane = {
+  name : "Arcane Sigil",
+  id: 2,
+  drops: 0,
+  amount: 1,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+var spectral = {
+  name : "Spectral Sigil",
+  id: 3,
+  drops: 0,
+  amount: 1,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+var shield = {
+  name : "Spirit Shield",
+  id: 4,
+  drops: 0,
+  amount: 1,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+var elixer = {
+  name : "Holy Elixer",
+  id: 5,
+  drops: 0,
+  amount: 1,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+
+var ess = {
+  name : "Pure Essence",
+  id: 6,
+  drops: 0,
+  amount: 2500,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+var law = {
+  name : "Law Runes",
+  id: 7,
+  drops: 0,
+  amount: 250,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+var cosmic = {
+  name : "Cosmic Runes",
+  id: 8,
+  drops: 0,
+  amount: 500,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+var death = {
+  name : "Death Runes",
+  id: 9,
+  drops: 0,
+  amount: 300,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+var soul = {
+  name : "Soul Runes",
+  id: 10,
+  drops: 0,
+  amount: 250,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+
+var cball = {
+  name : "Cannonballs",
+  id: 11,
+  drops: 0,
+  amount: 2000,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+var air = {
+  name : "Mystic Air Staff",
+  id: 12,
+  drops: 0,
+  amount: 1,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+var water = {
+  name : "Mystic Water Staff",
+  id: 13,
+  drops: 0,
+  amount: 1,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+var earth = {
+  name : "Mystic Earth Staff",
+  id: 14,
+  drops: 0,
+  amount: 1,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+var fire = {
+  name : "Mystic Fire Staff",
+  id: 15,
+  drops: 0,
+  amount: 1,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+
+var aarrows = {
+  name : "Adamant Arrows",
+  id: 16,
+  drops: 0,
+  amount: 750,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+var rbolts = {
+  name : "Runite Bolts",
+  id: 17,
+  drops: 0,
+  amount: 250,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+var obolts = {
+  name : "Onyx Bolts(e)",
+  id: 18,
+  drops: 0,
+  amount: 175,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+var top = {
+  name : "Mystic Robe Top",
+  id: 19,
+  drops: 0,
+  amount: 1,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+var bottom = {
+  name : "Mystic Robe Bottom",
+  id: 20,
+  drops: 0,
+  amount: 1,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+
+var aaore = {
+  name : "Adamantite Ore",
+  id: 15,
+  drops: 0,
+  amount: 125,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+var rore = {
+  name : "Runite Ore",
+  id: 15,
+  drops: 0,
+  amount: 20,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+var abar = {
+  name : "Adamantite Bar",
+  id: 15,
+  drops: 0,
+  amount: 35,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+var tplank = {
+  name : "Teak Planks",
+  id: 15,
+  drops: 0,
+  amount: 100,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+var dhides = {
+  name : "Green Dragonhides",
+  id: 15,
+  drops: 0,
+  amount: 100,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+
+var mahog = {
+  name : "Mahogany Logs",
+  id: 15,
+  drops: 0,
+  amount: 150,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+var magic = {
+  name : "Magic Logs",
+  id: 15,
+  drops: 0,
+  amount: 75,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+var rdt = {
+  name : "Uncuts drop",
+  id: 15,
+  drops: 0,
+  amount: 1,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+var shark = {
+  name : "Raw Sharks",
+  id: 15,
+  drops: 0,
+  amount: 70,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+var tuna = {
+  name : "Tuna Potatoes",
+  id: 15,
+  drops: 0,
+  amount: 30,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+
+var horns = {
+  name : "Desert Goat Horns",
+  id: 15,
+  drops: 0,
+  amount: 120,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+var berries = {
+  name : "White Berries",
+  id: 15,
+  drops: 0,
+  amount: 120,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+var Watermelon Seed = {
+  name : "Watermelon Seeds",
+  id: 15,
+  drops: 0,
+  amount: 24,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+var rseeds = {
+  name : "Ranarr Seeds",
+  id: 15,
+  drops: 0,
+  amount: 10,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+var anti = {
+  name : "Antidote++(4)",
+  id: 15,
+  drops: 0,
+  amount: ,
+  add: function(){
+    this.drops = this.drops + 1;
+  }
+  list: function(){
+    if (drops == 0){
+      return "";
+    }else{
+      return this.name + " x " + (this.drops * this.amount);
+    }
+  }
+};
+
+var text = 'some text';
+
+function darken(){
+  document.getElementById("ely").style.backgroundColor = "black";
+}
